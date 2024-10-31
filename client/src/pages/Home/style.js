e1eefa97475f8b174b37d585a6c1ab2036087f30
maarcotulio <marcotuliotavaresoliveira@gmail.com>
2024-10-31 17:31:24 -0300

    Css changes

--- client/src/pages/Home/style.js
+++ client/src/pages/Home/style.js
@@ -4,10 +4,11 @@ export const Container = styled.div`
   max-width: 600px;
   width: 100%;
   margin: auto;
-  padding-top: 16px;
+  margin-top: 60px;
   height: auto;
 
   h1 {
     text-align: center;
+    margin-bottom: 8px;
   }
 `;
